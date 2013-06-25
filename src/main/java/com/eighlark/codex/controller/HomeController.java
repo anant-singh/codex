70f9e4dec1a281e2f8c134a38fdfa19d273621cb
anant-singh <anant_xinix@yahoo.co.in>
2013-06-25 15:24:11 +0530

    Adding internationalisation support

--- src/main/java/com/eighlark/codex/controller/HomeController.java
+++ src/main/java/com/eighlark/codex/controller/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping({"/", "/home"})
+    @RequestMapping(value = "/home")
     public String showHomePage() {
         return "home";
     }
