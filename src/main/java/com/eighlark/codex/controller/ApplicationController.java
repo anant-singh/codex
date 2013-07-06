d25b99c1d78d9d92f43b5029ae7a63531dc2ab58
anant-singh <anant_xinix@yahoo.co.in>
2013-07-07 00:15:16 +0530

    CDX-GUI_i18n
    Base i18n configuration and properties files added

--- src/main/java/com/eighlark/codex/controller/ApplicationController.java
+++ src/main/java/com/eighlark/codex/controller/ApplicationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ApplicationController {
 
-    @RequestMapping(value = "/")
+    //@RequestMapping(value = "/")
     public String index(ModelMap map) {
         return "index";
     }
--- src/main/java/com/eighlark/codex/controller/HomeController.java
+++ src/main/java/com/eighlark/codex/controller/HomeController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/home")
+    @RequestMapping({"/home", "/"})
     public String showHomePage() {
-        return "home";
+        return "index";
     }
 }
